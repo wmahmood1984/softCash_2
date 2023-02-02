26384a02c41e32101965302078155ebb44177e5d
blockchainer2022 <91312709+blockchainer2022@users.noreply.github.com>
2023-02-02 14:57:02 +0530

    Update index.js

--- src/screens/LaunchPadList/index.js
+++ src/screens/LaunchPadList/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../../components/Banner";
 import LaunchPad from "./components/LaunchPad";
 
 const LaunchPadList = () => {
   return (
-    <div className=" text-white ">
-      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className=" text-white grid grid-cols-1">
+      <Banner />
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
         <LaunchPad />
         <LaunchPad />
         <LaunchPad />
